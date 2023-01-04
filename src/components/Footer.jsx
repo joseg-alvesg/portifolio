915729845e8bdeb3c79ed5bf414f90712b934b21
joseg-alvesg <cinhoalves85@gmail.com>
2023-01-03 23:45:36 -0300

    responsividade do header e da pagina HOME para mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,16 @@ import React, { Component } from 'react'
 import styles from './Footer.module.css';
 
 export default class Footer extends Component {
+  state = {
+    curD: new Date().toLocaleDateString(),
+  }
+
   render() {
     return (
-      <footer className={ styles.footer }>Footer</footer>
+      <footer className={ styles.footer }>
+        <section>{this.state.curD}</section>
+        <section>José alves | 2022</section>
+      </footer>
     )
   }
 }
--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -1,17 +1,23 @@
+/* BIBLIOTECAS */
 import React, { Component } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { A11y, Navigation, Pagination } from 'swiper';
+import { BsGithub } from 'react-icons/bs';
+
+/* COMPONENTES */
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import { data } from '../data/infos';
-import trybetunesPrint from '../data/images/trybetunes-foto.png';
-import styles from './styles/projetos.module.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { A11y, Navigation, Pagination, Scrollbar } from 'swiper';
 
+/* ESTILOS */
+import styles from './styles/projetos.module.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { BsGithub } from 'react-icons/bs';
+
+/* DATA */
+import { data } from '../data/infos';
+import trybetunesPrint from '../data/images/trybetunes-foto.png';
+
 
 export default class Projetos extends Component {
   render() {
@@ -22,12 +28,11 @@ export default class Projetos extends Component {
         <div className={ styles.projectContainer }>
           <section className={ styles.section }>
             <Swiper
-              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              modules={[Navigation, Pagination, A11y]}
               spaceBetween={50}
-              slidesPerView={3}
+              slidesPerView={1}
               navigation
               pagination={{ clickable: true }}
-              scrollbar={{ draggable: true }}
             >
               <SwiperSlide>
                 <div className={ styles.element }>
